0f84027365cd6559901339051522e74cc1224849
tbd-shafa <shafa@technobd.com>
2024-08-30 12:17:12 +0600

    mail send when cart checkou page email

--- catalog/controller/checkout/checkout.php
+++ catalog/controller/checkout/checkout.php
@@ -14,7 +14,7 @@ class Checkout extends \Opencart\System\Engine\Controller
 	 */
 	public function index(): void
 	{
-		
+
 		// Validate cart has products and has stock.
 		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
 			$this->response->redirect($this->url->link('checkout/cart', 'language=' . $this->config->get('config_language')));
@@ -76,7 +76,7 @@ class Checkout extends \Opencart\System\Engine\Controller
 			$data['shipping_method'] = '';
 		}
 		$products = $this->cart->getProducts();
-       
+
 		foreach ($products as &$product) {
 			$cart_id = $product['cart_id'];
 			$product_id = $product['product_id'];
@@ -121,7 +121,59 @@ class Checkout extends \Opencart\System\Engine\Controller
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
+		// Prepare and send email
+		if ($this->cart->hasProducts()) {
+			$store_info = $this->model_setting_store->getStore($this->config->get('config_store_id'));
+			$store_name = $store_info ? $store_info['name'] : $this->config->get('config_name');
+			$customer_email = $this->customer->isLogged() ? $this->customer->getEmail() : 'default@example.com';
+			$customer_first_name = $this->customer->isLogged() ? $this->customer->getFirstName() : 'Guest';
+			$customer_last_name = $this->customer->isLogged() ? $this->customer->getLastName() : '';
 
+			$subject = 'Your Product is in Checkout Page';
+			$message = 'Hi ' . $customer_first_name . ' ' . $customer_last_name . ',<br><br>';
+			$message .= 'You have the following products in the checkout page:<br><br>';
+
+			foreach ($data['products'] as $product) {
+				$color_name = '';
+				if ($product['selected_color']) {
+					$color_query = $this->db->query("SELECT name FROM " . DB_PREFIX . "color WHERE color_id = '" . (int)$product['selected_color'] . "'");
+					if ($color_query->num_rows) {
+						$color_name = $color_query->row['name'];
+					}
+				}
+
+				$message .= 'Product Name: ' . $product['name'] . '<br>';
+				$message .= 'Quantity: ' . $product['quantity'] . '<br>';
+				if (!empty($color_name)) {
+					$message .= 'Selected Color: ' . $color_name . '<br>';
+				}
+				$message .= '<br>';
+			}
+
+			$message .= '<br>Best regards,<br>' . $store_name;
+
+			$mail_option = [
+				'parameter' => $this->config->get('config_mail_parameter'),
+				'smtp_hostname' => $this->config->get('config_mail_smtp_hostname'),
+				'smtp_username' => $this->config->get('config_mail_smtp_username'),
+				'smtp_password' => html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8'),
+				'smtp_port' => $this->config->get('config_mail_smtp_port'),
+				'smtp_timeout' => $this->config->get('config_mail_smtp_timeout')
+			];
+
+			// Initialize the Mail class with options
+			$mail = new \Opencart\System\Library\Mail($this->config->get('config_mail_engine'), $mail_option);
+
+			// Set up the email details
+			$mail->setTo($customer_email);
+			$mail->setFrom($this->config->get('config_email'));
+			$mail->setSender(html_entity_decode($store_name, ENT_QUOTES, 'UTF-8'));
+			$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
+			$mail->setHtml($message);
+
+			// Send the email
+			$mail->send();
+		}
 		$this->response->setOutput($this->load->view('checkout/checkout', $data));
 	}
 }
